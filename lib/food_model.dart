104cbcccee9a1f36b49602b4d21db39945f51b60
Hao Nguyen Ngo <haongo28.dev@gmail.com>
2025-01-03 16:35:28 +0700

    add food model

---
+++ lib/food_model.dart
@@ -0,0 +1,12 @@
+class FoodModel {
+  final String id;
+  final String nameOfFood;
+  final int quanity;
+  final double price;
+  FoodModel({
+    required this.id, 
+    required this.nameOfFood, 
+    required this.quanity, 
+    required this.price, 
+  });
+}
